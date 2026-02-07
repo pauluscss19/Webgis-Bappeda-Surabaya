64b9e3833a68d155e00074222c5ce5a6dcbb4cc9
Pauluscss19 <23081010275@student.upnjatim.ac.id>
2026-02-07 10:34:37 +0700

    seeder admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User; // Pastikan namespace model User sesuai dengan aplikasi Anda
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Menggunakan updateOrCreate untuk mencegah duplikasi jika seeder dijalankan ulang
+        User::updateOrCreate(
+            ['email' => 'admin@surabaya.go.id'], // Kunci pencarian (Cek apakah email ini ada)
+            [
+                'name' => 'Administrator Surabaya', // Nama default
+                'password' => Hash::make('admin123'), // Password wajib di-hash
+                'email_verified_at' => now(), // Langsung verifikasi email
+            ]
+        );
+    }
+}
